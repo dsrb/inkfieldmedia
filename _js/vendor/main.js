33539fcc18a2e09bde3dec7721dc8d47911f5ce9
dsrb <jahl@dsrb.com.au>
2017-02-02 13:44:38 +1000

    More Style
    Changed footer style, fixed JS issues, placed site logo and fixed fonts

---
+++ _js/vendor/main.js
@@ -0,0 +1,27 @@
+function sticky_relocate() {
+  var window_top = $(window).scrollTop();
+  var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top)
+        $('#sticky-element').addClass('sticky');
+    else
+        $('#sticky-element').removeClass('sticky');
+}
+
+$(function() {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
+
+function spacer_add() {
+  var window_top = $(window).scrollTop();
+  var div_top = $('#spacer-anchor').offset().top;
+    if (window_top > div_top)
+        $('#spacer-element').addClass('spacer');
+    else
+        $('#spacer-element').removeClass('spacer');
+}
+
+$(function() {
+    $(window).scroll(spacer_add);
+    spacer_add();
+});
